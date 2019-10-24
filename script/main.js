33671931e3731c53d3f1cf3d570029dc50b13f06
SethRollins2k19 <46203101+SethRollins2k19@users.noreply.github.com>
2019-10-25 01:26:04 +0300

    second versted block

---
+++ script/main.js
@@ -0,0 +1,28 @@
+'use strict'
+
+let dataPhones = [
+{	
+	imgUrl: 'files/Xiaomi-Redmi-Note-8-Pro'
+	name: 'XIAOMI';
+	description: 'Смартфон Xiaomi Redmi';
+	price: 19600;
+	reaiting: 3.5;
+	reviews: 13;
+},
+{
+	name: 'XIAOMI';
+	description: 'Смартфон Xiaomi Redmi';
+	price: 17600;
+	reaiting: 4.5;
+	reviews: 17;
+},
+{
+	name: 'XIAOMI';
+	description: 'Смартфон Xiaomi Redmi';
+	price: 17600;
+	reaiting: 4.5;
+	reviews: 17;
+}
+];
+
+let CreateElement = function (nameTag, tagClass, tagTitle)
