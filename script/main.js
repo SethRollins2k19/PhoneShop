3c6bd4d1782a10262dfff8796047348da5bb0532
SethRollins2k19 <46203101+SethRollins2k19@users.noreply.github.com>
2019-10-26 11:52:47 +0300

    all functional is ready

--- script/main.js
+++ script/main.js
@@ -2,27 +2,120 @@
 
 let dataPhones = [
 {	
-	imgUrl: 'files/Xiaomi-Redmi-Note-8-Pro'
-	name: 'XIAOMI';
-	description: 'Смартфон Xiaomi Redmi';
-	price: 19600;
-	reaiting: 3.5;
-	reviews: 13;
+	imgUrl: 'files/Xiaomi-Redmi-Note-8-Pro.jpg',
+	name: 'XIAOMI',
+	description: 'Смартфон Xiaomi Redmi',
+	price: 19600,
+	reaiting: 3.5,
+	reviews: 13,
 },
 {
-	name: 'XIAOMI';
-	description: 'Смартфон Xiaomi Redmi';
-	price: 17600;
-	reaiting: 4.5;
-	reviews: 17;
+	imgUrl: 'files/Xiaomi-Redmi-Note-8-Pro.jpg',
+	name: 'XIAOMI',
+	description: 'Смартфон Xiaomi Redmi',
+	price: 18600,
+	reaiting: 4.5,
+	reviews: 17,
 },
-{
-	name: 'XIAOMI';
-	description: 'Смартфон Xiaomi Redmi';
-	price: 17600;
-	reaiting: 4.5;
-	reviews: 17;
+{	
+	imgUrl: 'files/Xiaomi-Redmi-Note-8-Pro.jpg',
+	name: 'XIAOMI',
+	description: 'Смартфон Xiaomi Redmi',
+	price: 17600,
+	reaiting: 4.5,
+	reviews: 17,
+},
+{	
+	imgUrl: 'files/Xiaomi-Redmi-Note-8-Pro.jpg',
+	name: 'XIAOMI',
+	description: 'Смартфон Xiaomi Redmi',
+	price: 13600,
+	reaiting: 4.5,
+	reviews: 17,
+},
+{	
+	imgUrl: 'files/Xiaomi-Redmi-Note-8-Pro.jpg',
+	name: 'XIAOMI',
+	description: 'Смартфон Xiaomi Redmi',
+	price: 17600,
+	reaiting: 4.5,
+	reviews: 17,
+},
+{	
+	imgUrl: 'files/Xiaomi-Redmi-Note-8-Pro.jpg',
+	name: 'XIAOMI',
+	description: 'Смартфон Xiaomi Redmi',
+	price: 127600,
+	reaiting: 4.5,
+	reviews: 17,
 }
 ];
 
-let CreateElement = function (nameTag, tagClass, tagTitle)
+let createElement = function (nameTag, tagClass, tagTitle){
+	let tag = document.createElement(nameTag);
+	tag.classList.add(tagClass);
+	if(tagTitle){
+		tag.textContent = tagTitle;
+	}
+	return tag;
+}
+
+function createCard (aboutPhone){
+	let section = createElement('section','card-phone');
+	let img = createElement('img','card-img');
+	img.src = aboutPhone.imgUrl;
+	let h3 = createElement('h3','phone-brand-name',aboutPhone.name);
+	let phoneName = createElement('p','phone-name',aboutPhone.description);
+	let price = createElement('p','price',aboutPhone.price);
+	
+	let reviews = createElement('div','reviews');
+	let spanRate = createElement('span','span-rate',aboutPhone.reaiting);
+	let spanReviews = createElement('span','span-review',aboutPhone.reviews + ' отзывов');
+	reviews.appendChild(spanRate);
+	reviews.appendChild(spanReviews);
+	section.appendChild(img);
+	section.appendChild(h3);
+	section.appendChild(phoneName);
+	section.appendChild(price);
+	section.appendChild(reviews);
+
+	return section;
+}
+function proccessingCards (dataAboutPhones){
+	let phoneBlocks = document.querySelector('.devisec-part');
+	for (let i = 0; i < dataAboutPhones.length;i++){
+		phoneBlocks.appendChild(createCard(dataAboutPhones[i]));
+	}
+}
+function prepareForReload (classNameForReload){
+	let tagsForReload = document.querySelectorAll(classNameForReload);
+	for (let tag = 0 ; tag < tagsForReload.length; tag++){
+		tagsForReload[tag].remove();
+	}
+}
+
+function sortByPrice(massivePhones,paramForSort) {
+	for (let i = 0; i < massivePhones.length; i++){
+		for (let j = 1; j < massivePhones.length; j++){
+			if (massivePhones[j-1][paramForSort] > massivePhones[j][paramForSort]){
+				let pass = massivePhones[j];
+				massivePhones[j] = massivePhones[j-1];
+				massivePhones[j-1] = pass; 
+			}
+		}
+	}
+	return massivePhones;
+}
+let IsActiveFilterByPrice = false;
+function renderFilter(dataAboutPhones,filterParam){
+	if(IsActiveFilterByPrice === false){
+		prepareForReload('.card-phone');
+		proccessingCards(sortByPrice(dataAboutPhones,filterParam));
+		IsActiveFilterByPrice = true;
+	} 
+	else {
+		prepareForReload('.card-phone');
+		proccessingCards(sortByPrice(dataAboutPhones,filterParam).reverse());
+		IsActiveFilterByPrice = false;
+	}
+}
